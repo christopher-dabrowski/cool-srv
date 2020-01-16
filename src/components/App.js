fb6825f8d8bd25372d7917289ec6b127f19ad90e
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-01-16 14:33:48 +0100

    Add router to project

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import React from "react";
 import "../styles/App.css";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <Router>
+
+      </Router>
       Nice servers and happy admins.
     </div>
   );
