a1b7f036dd0ecb779ea7340770652a9607748d1f
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-01-16 14:26:29 +0100

    Cleanup app template

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      Nice servers and happy admins.
+    </div>
+  );
+}
+
+export default App;
