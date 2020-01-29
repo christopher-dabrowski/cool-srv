6e11bf9da6271ccf7f0fdf7d8f1dce62f1dca485
WinCubeML <mati031398@gmail.com>
2020-01-30 00:15:55 +0100

    Modal changes

--- front/src/components/Networks.js
+++ front/src/components/Networks.js
@@ -1,13 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Button, Modal, Form } from "reactstrap";
+import { Link, BrowserRouter as Router, Route, withRouter, Switch } from "react-router-dom";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Form, FormGroup, Label, Input } from "reactstrap";
+import NetworkModal from "./modals/NetworkModal";
 
-const Networks = ({ className }) => {
-  // TODO: Move to Redux
-  const [show, setShow] = React.useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+const Networks = ({ className, match }) => {
   return (
     <div className={className}>
       <h2 className="text-center">Networks</h2>
@@ -19,58 +16,18 @@ const Networks = ({ className }) => {
           Network 2
         </li>
       </ul>
-      <Button variant="primary" onClick={handleShow}>
-        Modal
-      </Button>
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Dodaj sieć</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form>
-            <Form.Group controlId="ipAddress">
-              <Form.Label>Adres IP</Form.Label>
-              <Form.Control type="text" placeholder="Podaj adres IP" />
-            </Form.Group>
-            <Form.Group controlId="mask">
-              <Form.Label>Maska podsieci</Form.Label>
-              <Form.Control type="text" placeholder="Podaj maskę podsieci" />
-            </Form.Group>
-            <Form.Group controlId="desc">
-              <Form.Label>Opis</Form.Label>
-              <Form.Control type="text" placeholder="Podaj opis" />
-            </Form.Group>
-            <Form.Group controlId="dnsAddress">
-              <Form.Label>Adres DNS</Form.Label>
-              <Form.Control type="text" placeholder="Podaj adres DNS" />
-            </Form.Group>
-            <Form.Group controlId="location">
-              <Form.Label>Lokalizacja fizyczna</Form.Label>
-              <Form.Control type="text" placeholder="Podaj lokalizację fizyczną" />
-            </Form.Group>
-            <Form.Group controlId="routable">
-              <Form.Label>Rutowalne?</Form.Label>
-              <Form.Control type="text" placeholder="Routable?" />
-            </Form.Group>
-            <Form.Group controlId="public">
-              <Form.Label>Publiczne?</Form.Label>
-              <Form.Control type="text" placeholder="Public?" />
-            </Form.Group>
-            <Form.Group controlId="dynamic">
-              <Form.Label>Adres dynamiczny?</Form.Label>
-              <Form.Control type="text" placeholder="Dynamic?" />
-            </Form.Group>
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
+      <Link to={`${match.url}/create`}>
+        <Button color="primary">
+          Modal
+        </Button>
+      </Link>
+      <Router>
+        <Switch>
+          <Route path="/networks/create">
+            <NetworkModal />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 
@@ -80,4 +37,4 @@ Networks.propTypes = {
   className: PropTypes.string
 };
 
-export default Networks;
+export default withRouter(Networks);
---
+++ front/src/components/modals/DevicesModal.js
@@ -0,0 +1,62 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Link, BrowserRouter as Router, Route, withRouter, Switch } from "react-router-dom";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Form, FormGroup, Label, Input } from "reactstrap";
+
+const DevicesModal = ({ match }) => {
+	return (
+		<Modal isOpen={true}>
+			<ModalHeader closeButton>
+				Dodaj sieć
+            </ModalHeader>
+			<ModalBody>
+				<Form>
+					<FormGroup>
+						<Label>Adres IP</Label>
+						<Input type="text" placeholder="Podaj adres IP" />
+					</FormGroup>
+					<FormGroup>
+						<Label>Maska podsieci</Label>
+						<Input type="text" placeholder="Podaj maskę podsieci" />
+					</FormGroup>
+					<FormGroup>
+						<Label>Opis</Label>
+						<Input type="text" placeholder="Podaj opis" />
+					</FormGroup>
+					<FormGroup>
+						<Label>Adres DNS</Label>
+						<Input type="text" placeholder="Podaj adres DNS" />
+					</FormGroup>
+					<FormGroup>
+						<Label>Lokalizacja fizyczna</Label>
+						<Input type="text" placeholder="Podaj lokalizację fizyczną" />
+					</FormGroup>
+					<FormGroup>
+						<Label>Rutowalne?</Label>
+						<Input type="text" placeholder="Routable?" />
+					</FormGroup>
+					<FormGroup>
+						<Label>Publiczne?</Label>
+						<Input type="text" placeholder="Public?" />
+					</FormGroup>
+					<FormGroup>
+						<Label>Adres dynamiczny?</Label>
+						<Input type="text" placeholder="Dynamic?" />
+					</FormGroup>
+				</Form>
+			</ModalBody>
+			<ModalFooter>
+				<Link to={`${match.url}`}>
+					<Button color="secondary">
+						Close
+                    </Button>
+				</Link>
+				<Button color="primary">
+					Save Changes
+                </Button>
+			</ModalFooter>
+		</Modal>
+	);
+};
+
+export default withRouter(DevicesModal);
