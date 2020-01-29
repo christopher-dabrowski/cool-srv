1e134ef4a5a3541e0fb958a6cdde2f0fcf57b4d6
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-01-29 16:11:10 +0100

    Working navigation

--- front/src/components/App.js
+++ front/src/components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles/App.css";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import Header from "./Layout/Header";
 import NavigationDrawer from "./Layout/NavigationDrawer";
 import Networks from "./Networks";
@@ -13,7 +13,15 @@ function App() {
 
         <main className="d-flex">
           <NavigationDrawer />
-          <Networks className="flex-grow-1" />
+
+          <Switch>
+            <Route exact path="/">
+              <Redirect to="/networks" />
+            </Route>
+            <Route path="/networks">
+              <Networks className="flex-grow-1" />
+            </Route>
+          </Switch>
         </main>
 
       </Router>
--- front/src/components/Layout/NavigationDrawer.js
+++ front/src/components/Layout/NavigationDrawer.js
@@ -1,31 +1,28 @@
 import React from "react";
-import { Nav, NavItem, NavLink } from "reactstrap";
+import { Nav, NavItem, Button } from "reactstrap";
+import { NavLink } from "react-router-dom";
 
 const NavigationDrawer = () => {
 
-  const style = {
-    width: "20%"
-  };
-
   return (
-    <Nav vertical pills className="bg-gradient-blue shadow rounded" style={{ width: "20%", height: "80vh" }}>
+    <Nav vertical pills className="bg-gradient-gray shadow rounded" style={{ width: "20%", height: "80vh" }}>
       <NavItem className="pr-0">
-        <NavLink active>
+        <NavLink className="nav-link" to="/networks">
           Networks
         </NavLink>
       </NavItem>
       <NavItem className="pr-0">
-        <NavLink>
+        <NavLink className="nav-link" to="/devices">
           Devices
         </NavLink>
       </NavItem>
       <NavItem className="pr-0">
-        <NavLink>
+        <NavLink className="nav-link" to="/nat">
           NAT
         </NavLink>
       </NavItem>
       <NavItem className="pr-0">
-        <NavLink>
+        <NavLink className="nav-link" to="/vlan">
           VLAN
         </NavLink>
       </NavItem>
