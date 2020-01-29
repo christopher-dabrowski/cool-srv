20afbf0e589a9f9f2f563bf53160dcf40cd25af5
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-01-29 08:03:57 +0100

    Page header

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "../styles/App.css";
 import { BrowserRouter as Router } from "react-router-dom";
+import Header from './Layout/Header';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header />
+
       </Router>
       Nice servers and happy admins.
     </div>
---
+++ src/components/Layout/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Navbar, NavbarBrand } from "reactstrap";
+import { Link } from "react-router-dom"
+
+const Header = () => {
+
+  return (
+    <Navbar color="light" className="navbar-light">
+      <Link className="navbar-brand" to="/">
+        <span><b>CS</b> - Cool Server</span>
+      </Link>
+    </Navbar>
+  );
+};
+
+export default Header;
