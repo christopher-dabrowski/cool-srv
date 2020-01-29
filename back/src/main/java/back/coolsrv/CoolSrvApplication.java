d7e59c3c214d6b2b1801c6266bb3b99d3f4b5e63
WinCubeML <mati031398@gmail.com>
2020-01-29 11:50:44 +0100

    init for backend

---
+++ back/src/main/java/back/coolsrv/CoolSrvApplication.java
@@ -0,0 +1,13 @@
+package back.coolsrv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoolSrvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoolSrvApplication.class, args);
+    }
+
+}
---
+++ back/src/test/java/back/coolsrv/CoolSrvApplicationTests.java
@@ -0,0 +1,13 @@
+package back.coolsrv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoolSrvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
