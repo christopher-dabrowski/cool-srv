17dfb783c712b425911e11427e87b9a58d53c8d1
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-01-30 02:00:20 +0100

    Init Vlan

--- back/src/main/java/back/coolsrv/components/InitDataBase.java
+++ back/src/main/java/back/coolsrv/components/InitDataBase.java
@@ -1,6 +1,7 @@
 package back.coolsrv.components;
 
 import back.coolsrv.enities.Network;
+import back.coolsrv.enities.Vlan;
 import back.coolsrv.repositories.DeviceRepository;
 import back.coolsrv.repositories.NATRepository;
 import back.coolsrv.repositories.NetworkRepository;
@@ -10,6 +11,8 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service
 public class InitDataBase {
@@ -30,7 +33,8 @@ public class InitDataBase {
         Network network = new Network("12.12.12.12", "32", "opis", "dns<3", null, false, true, false);
         networkRepository.save(network);
 
-//        Vlan vlan = new Vlan("vlan", new ArrayList<>(), "desc");
+        Vlan vlan = new Vlan("vlan", new ArrayList<>(), "desc");
+        vlanRepository.save(vlan);
 //        Device device = new Device("123.45.67.89", "hostname", "opis", "11:11:11:11:11:11", true, "tam", vlan);
 //        Network network = new Network("12.34.5.6", "24", "opis", "11.0.0.0", "tutaj", true, false, true);
 //        NAT nat = new NAT("nazwa", device, "opis", "123.55.55.0", network);
