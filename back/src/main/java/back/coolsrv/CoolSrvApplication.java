eeb089bb6de755256131e53c886fb819659ca464
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-01-30 01:57:34 +0100

    DB init works?

--- back/src/main/java/back/coolsrv/CoolSrvApplication.java
+++ back/src/main/java/back/coolsrv/CoolSrvApplication.java
@@ -1,10 +1,17 @@
 package back.coolsrv;
 
+import back.coolsrv.components.InitDataBase;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class CoolSrvApplication {
+
+    @Autowired
+    private InitDataBase initDataBase;
 
     public static void main(String[] args) {
         SpringApplication.run(CoolSrvApplication.class, args);
--- back/src/main/java/back/coolsrv/components/InitDataBase.java
+++ back/src/main/java/back/coolsrv/components/InitDataBase.java
@@ -1,5 +1,6 @@
 package back.coolsrv.components;
 
+import back.coolsrv.enities.Network;
 import back.coolsrv.repositories.DeviceRepository;
 import back.coolsrv.repositories.NATRepository;
 import back.coolsrv.repositories.NetworkRepository;
@@ -8,8 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 public class InitDataBase {
     @Autowired
     DeviceRepository deviceRepository;
@@ -25,6 +27,9 @@ public class InitDataBase {
 
     @EventListener(ApplicationReadyEvent.class)
     public void run() throws Exception {
+        Network network = new Network("12.12.12.12", "32", "opis", "dns<3", null, false, true, false);
+        networkRepository.save(network);
+
 //        Vlan vlan = new Vlan("vlan", new ArrayList<>(), "desc");
 //        Device device = new Device("123.45.67.89", "hostname", "opis", "11:11:11:11:11:11", true, "tam", vlan);
 //        Network network = new Network("12.34.5.6", "24", "opis", "11.0.0.0", "tutaj", true, false, true);
