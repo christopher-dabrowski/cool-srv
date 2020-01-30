ef52ae22fe3899cb197e6034c3c117b29aff1230
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-01-30 04:07:29 +0100

    Redux for adding message

--- front/src/components/reducers/index.js
+++ front/src/components/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import networksReducer from "./networksReducer";
+import messagesReducer from "./messagesReducer";
 
 const rootReducer = combineReducers({
-  networks: networksReducer
+  networks: networksReducer,
+  messages: messagesReducer
 });
 
 export default rootReducer;
