82fd948a54b32d6e5bcf66f42befd90416b76929
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-01-30 03:07:22 +0100

    Edit buttons

--- front/src/components/lists/NetworkListItem.js
+++ front/src/components/lists/NetworkListItem.js
@@ -1,12 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Button } from "reactstrap";
 import { ListGroupItem } from "reactstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
 
 const NetworkListItem = ({ network }) => {
   console.log(network);
 
   return (
-    <ListGroupItem>{network.address}</ListGroupItem>
+    <ListGroupItem className="d-flex align-items-baseline">
+      <b className="flex-grow-1">
+        {network.address}
+      </b>
+
+      <section>
+        <Button color="info">
+          <FontAwesomeIcon icon={faEdit} className="mr-2" />
+          Edit
+        </Button>
+        <Button color="danger">
+          <FontAwesomeIcon icon={faTrash} className="mr-2" />
+          Delete
+        </Button>
+      </section>
+    </ListGroupItem>
   );
 };
 
